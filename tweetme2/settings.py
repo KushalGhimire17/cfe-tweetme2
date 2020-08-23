8b58e1fd7283d1e25c6ddbeb5472cc02816aee04
KushalGhimire17 <kushalghimire61@gmail.com>
2020-08-23 21:12:27 +0545

    Postgresql database setup

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -75,9 +75,11 @@ WSGI_APPLICATION = 'tweetme2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tweetme_project',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'    }
 }
 
 
